996cd845828dfe071b8f6c2df45de451b03d7856
Ashish Verma <ashish1500616@gmail.com>
2017-11-05 11:22:54 +0530

    Add DisplayBoard Function

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -13,3 +13,13 @@ def reset_board():
 	board=[' ']*10
 	game_state=True
 
+# function to display the board
+def display_board():
+	clear()
+    print "  "+board[7]+" |"+board[8]+" | "+board[9]+" "
+    print "------------"
+    print "  "+board[4]+" |"+board[5]+" | "+board[6]+" "
+    print "------------"
+    print "  "+board[1]+" |"+board[2]+" | "+board[3]+" "
+
+	
