bbb2b8840cbc5228cbdf637df10e283e93300778
Ashish Verma <ashish1500616@gmail.com>
2017-11-05 11:23:58 +0530

    Add Wincheck Function

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -22,4 +22,17 @@ def display_board():
     print "------------"
     print "  "+board[1]+" |"+board[2]+" | "+board[3]+" "
 
-	
+#function to check win_status
+def win_check(board, player):
+    ''' Check Horizontals,Verticals, and Diagonals for a win '''
+    if (board[7]  ==  board[8] ==  board[9] == player) or \
+        (board[4] ==  board[5] ==  board[6] == player) or \
+        (board[1] ==  board[2] ==  board[3] == player) or \
+        (board[7] ==  board[4] ==  board[1] == player) or \
+        (board[8] ==  board[5] ==  board[2] == player) or \
+        (board[9] ==  board[6] ==  board[3] == player) or \
+        (board[1] ==  board[5] ==  board[9] == player) or \
+        (board[3] ==  board[5] ==  board[7] == player):
+        return True
+    else:
+        return False
