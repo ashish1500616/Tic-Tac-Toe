6cffdcf4f8aacb69c0c0c2e33a19553186d829f4
Ashish Verma <ashish1500616@gmail.com>
2017-11-05 11:21:20 +0530

    Add Reset Function

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -6,3 +6,10 @@ clear =lambda :os.system('cls')
 board =[' '] * 10
 game_state=True
 announce =''
+
+# Reset Function
+def reset_board():
+	global board,game_state
+	board=[' ']*10
+	game_state=True
+
